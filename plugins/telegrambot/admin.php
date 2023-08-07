08228120758ac19cd10c59e4b8f0ac58b9aa1325
Olya <olapancisina1@gmail.com>
2023-08-07 10:56:34 +0200

    telegram bot 1.3

--- plugins/telegrambot/admin.php
+++ plugins/telegrambot/admin.php
@@ -21,7 +21,6 @@
         <?php
     }
 
-
     // -----------------Для токена та ID ----------
     // Додайте поля для введення токена та ID чату на сторінці налаштувань:
     function telegram_bot_plugin_init_settings_general() {
@@ -66,6 +65,54 @@
 
 
 
+    // ------для файлов----
+
+   
+    function display_php_files_list() {
+        $them_folder = get_template_directory();
+
+        $selected_files = get_option('my_plugin_settings_files', array());
+
+        if (empty($selected_files)) {
+            $selected_files = array('index.php');
+        }
+
+        if ($them_folder) {
+            $php_files = array();
+            $files = scandir($them_folder);
+
+            foreach ($files as $file) {
+                if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+                    $php_files[] = $file;
+                }
+            }
+    
+            foreach ($php_files as $php_file) {
+                ?>
+                <input type="checkbox" name="my_plugin_settings_files[]" value="<?php echo esc_attr($php_file); ?>" <?php checked(in_array($php_file, $selected_files)); ?> /> <?php echo esc_html($php_file); ?><br>
+                <?php
+            }
+        } else {
+            echo 'Theme folder not found.';
+        }
+    }
+    
+    function telegram_bot_plugin_init_settings_file() {
+        add_settings_section(
+            'telegram_bot_plugin_file_section',
+            'Select which pages your forms are on',
+            'display_php_files_list',
+            'telegram_bot_plugin_settings_file'
+        );  
+        
+        register_setting('telegram_bot_plugin_settings_file', 'my_plugin_settings_files');
+    }
+    add_action('admin_init', 'telegram_bot_plugin_init_settings_file');
+    
+
+    
+  
+
 
     // -----------------Для полей ----------
 
@@ -107,7 +154,10 @@
                 foreach ($textareas as $textarea) {
                     // Действия с каждым найденным textarea в форме
                     $name = $textarea->getAttribute('name');
-                    $inputData[] = $name; // Добавляем атрибуты <textarea> в массив
+                    if (!isset($inputData[$form_name])) {
+                        $inputData[$form_name] = array();
+                    }
+                    $inputData[$form_name][] = $name; // Добавляем атрибуты <textarea> в массив
                 }
             }
         } else {
@@ -129,6 +179,8 @@
     }
 
 
+
+
     // // Добавляем настройки поля в раздел Field Settings
     function telegram_bot_plugin_init_settings_field() {
         add_settings_section(
@@ -136,13 +188,14 @@
             'Оберіть, які поля надсилати в Telegram Bot:',
             'my_plugin_field_settings_callback',
             'telegram_bot_plugin_settings_field'
-        );    
+        );  
+          
         register_setting('telegram_bot_plugin_settings_field', 'my_plugin_settings_fields');
     }
-    add_action('admin_init', 'telegram_bot_plugin_init_settings_field');
+ add_action('admin_init', 'telegram_bot_plugin_init_settings_field');
     
 
-    // // Добавляем настройки поля в раздел Theme Settings
+
 
 
     // Сторінка налаштувань 'Telegram Bot'
@@ -151,7 +204,8 @@
         <div class="wrap">
             <h1>Telegram Bot Settings</h1>
             <h2 class="nav-tab-wrapper">
-                <a href="#general-settings" class="nav-tab nav-tab-active">Bot settings</a>
+                <a href="#general-settings" class="nav-tab nav-tab-active">Bot Settings</a>
+                <a href="#file-settings" class="nav-tab">Setting PHP Files</a>
                 <a href="#field-settings" class="nav-tab">Setting Fields</a>
             </h2>
     
@@ -167,6 +221,17 @@
             </div>
     
             <!-- Вміст 2 табу - Field Settings -->
+            <div id="file-settings" class="tab-content" style="display:none;">
+                <form method="post" action="options.php">
+                    <?php
+                    settings_fields('telegram_bot_plugin_settings_file');
+                    do_settings_sections('telegram_bot_plugin_settings_file');
+                    ?>
+                    <?php submit_button(); ?>
+                </form>
+            </div>
+
+            <!-- Вміст 3 табу - Field Settings -->
             <div id="field-settings" class="tab-content" style="display:none;">
                 <form method="post" action="options.php">
                     <?php
